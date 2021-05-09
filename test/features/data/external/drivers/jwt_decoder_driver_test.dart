36fa4f5534920da937dc17e3572c81b32cfeb6be
Munhoz <munhozfontana@gmail.com>
2021-05-09 09:41:20 -0300

    testes futuros

---
+++ test/features/data/external/drivers/jwt_decoder_driver_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('jwt decoder driver ...', (tester) async {
+    // TODO: Implement test
+  });
+}
---
+++ test/features/presentation/pages/devolution/devolution_page_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('devolution page ...', (tester) async {
+    // TODO: Implement test
+  });
+}
