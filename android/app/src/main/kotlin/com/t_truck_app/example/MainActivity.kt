ededb608a02ee26e006819c461932b0e6a20f87b
Munhoz <munhozfontana@gmail.com>
2021-05-28 17:14:31 -0300

    teste cielo v21

--- android/app/src/main/kotlin/com/t_truck_app/example/MainActivity.kt
+++ android/app/src/main/kotlin/com/t_truck_app/example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.t_truck_app
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import com.adapter.CieloChannel
 import com.adapter.apply.CieloPagamento
@@ -12,7 +13,22 @@ class MainActivity: FlutterActivity() {
         override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         
-        CieloChannel.CieloRun.setup(flutterEngine.dartExecutor.binaryMessenger, CieloPagamentoGSA(this));
+        CieloChannel.CieloRun.setup(flutterEngine.dartExecutor.binaryMessenger, cieloPagamentoGSA());
     }
-    
+
+    private fun cieloPagamentoGSA(): CieloChannel.CieloRun? {
+
+        try {
+            return CieloPagamentoGSA(this);
+        } catch (e: Exception) {
+            var response = CieloChannel.PayResponse();
+            Log.d("SDKClient", "#### VOLTANDO PARA O FLUTTER ####")
+            response.error = "ERROR CARAI"
+            throw Error("ERROR CARAI EXPTION")
+        }
+
+    }
+
+
+
 }
