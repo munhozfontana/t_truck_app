952d020d442fdb88f8b1f0b3c54ebec6552c2dfa
Munhoz <munhozfontana@gmail.com>
2021-05-29 14:26:21 -0300

    teste cielo v31

--- lib/test_cielo.dart
+++ lib/test_cielo.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:core';
 
 import 'package:flutter/cupertino.dart';
@@ -5,27 +6,48 @@ import 'package:flutter/material.dart';
 import 'package:t_truck_app/features/data/external/channels/cielo_channel.dart';
 import 'package:t_truck_app/features/data/external/channels/cielo_driver.dart';
 
-class TestCielo extends StatelessWidget {
+class TestCielo extends StatefulWidget {
+  @override
+  _TestCieloState createState() => _TestCieloState();
+}
+
+class _TestCieloState extends State<TestCielo> {
+  var onClick = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var credentials = CieloCredentials()
-            ..accessToken = 'asdasd'
-            ..clientID = 'asdasdasd';
+        onPressed: onClick
+            ? null
+            : () async {
+                setState(() {
+                  onClick = true;
+                });
 
-          var arg = PayParam()
-            ..reference = 'GSA'
-            ..cieloCredentials = credentials
-            ..sku = 'c2f5fb9a-5542-406e-8b79-17892329cda8'
-            ..description = 'produto fake'
-            ..unit_of_measure = 'EACH'
-            ..quantity = 1
-            ..unit_price = 1;
-          await CieloDriver().payCielo(arg);
-        },
+                var credentials = CieloCredentials()
+                  ..accessToken = 'asdasd'
+                  ..clientID = 'asdasdasd';
+
+                var arg = PayParam()
+                  ..reference = 'GSA'
+                  ..cieloCredentials = credentials
+                  ..sku = 'c2f5fb9a-5542-406e-8b79-17892329cda8'
+                  ..description = 'produto fake'
+                  ..unit_of_measure = 'EACH'
+                  ..quantity = 1
+                  ..unit_price = 1;
+                CieloDriver().payCielo(arg);
+
+                Timer(
+                    Duration(seconds: 5),
+                    () => {
+                          setState(() {
+                            onClick = false;
+                          })
+                        });
+              },
       ),
     );
   }
