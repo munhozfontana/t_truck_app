88952d617d6da71a180471dfe20df394508491df
Munhoz <munhozfontana@gmail.com>
2021-06-04 12:46:34 -0300

    criando model

--- lib/features/clients/list_products/domain/entites/product_entity.dart
+++ lib/features/clients/list_products/domain/entites/product_entity.dart
@@ -4,4 +4,25 @@ class ProductEntity {
   int? maxQuantity;
   int? quantity;
   bool hidden = false;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ProductEntity &&
+        other.id == id &&
+        other.name == name &&
+        other.maxQuantity == maxQuantity &&
+        other.quantity == quantity &&
+        other.hidden == hidden;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        maxQuantity.hashCode ^
+        quantity.hashCode ^
+        hidden.hashCode;
+  }
 }
