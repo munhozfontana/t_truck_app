3fb45276c3e2388d6b7d8d8c61504909d1db14a0
Munhoz <munhozfontana@gmail.com>
2021-06-05 13:37:33 -0300

    continua

--- lib/features/clients/list_clients/data/external/clients_api.dart
+++ lib/features/clients/list_clients/data/external/clients_api.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:t_truck_app/core/adapters/protocols/i_http_external.dart';
+import 'package:t_truck_app/core/adapters/protocols/i_logged_user.dart';
 import 'package:t_truck_app/features/clients/list_clients/data/models/clients_model.dart';
 
 mixin IClientAdapter {
@@ -7,13 +9,22 @@ mixin IClientAdapter {
 
 class ClientsApi implements IClientAdapter {
   IHttp iHttp;
+  ILoggedUser iLoggedUser;
 
   ClientsApi({
     required this.iHttp,
+    required this.iLoggedUser,
   });
 
   @override
-  Future<List<ClientModel>> getAll() {
-    return Future.value([]);
+  Future<List<ClientModel>> getAll() async {
+    try {
+      var res =
+          await iHttp.getHttp('${env['URL_BASE']}/order/${iLoggedUser.login}');
+      var clientModel = ClientModel.fromJson(res.body!);
+      return orderByIdentificacao;
+    } catch (e) {
+      throw ApiException(error: ApiMensages.Order_LIST_ERROR);
+    }
   }
 }
--- lib/features/clients/list_clients/data/models/clients_model.dart
+++ lib/features/clients/list_clients/data/models/clients_model.dart
@@ -1,3 +1,38 @@
+import 'dart:convert';
+
 import 'package:t_truck_app/features/clients/list_clients/domain/entites/client_entity.dart';
 
-class ClientModel extends ClientEntity {}
+class ClientModel extends ClientEntity {
+  int? codCli;
+  String? cliente;
+  int? qtde;
+
+  ClientModel({
+    this.codCli,
+    this.cliente,
+    this.qtde,
+  }) : super(
+          name: cliente,
+        );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'codCli': codCli,
+      'cliente': cliente,
+      'qtde': qtde,
+    };
+  }
+
+  factory ClientModel.fromMap(Map<String, dynamic> map) {
+    return ClientModel(
+      codCli: map['CODCLI'],
+      cliente: map['CLIENTE'],
+      qtde: map['QTDE'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ClientModel.fromJson(String source) =>
+      ClientModel.fromMap(json.decode(source));
+}
