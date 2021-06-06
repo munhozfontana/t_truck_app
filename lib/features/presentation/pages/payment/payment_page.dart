99ece3959976d1a22d769e95aa649d644e3aa048
Munhoz <munhozfontana@gmail.com>
2021-06-06 10:42:46 -0300

    adicao de validacoes

--- lib/features/presentation/pages/payment/payment_page.dart
+++ lib/features/presentation/pages/payment/payment_page.dart
@@ -30,13 +30,12 @@ class PaymentPage extends GetWidget<PaymentController> {
               Spacer(flex: 33),
               BtnPrimary(
                 label: 'Cartão de crédito',
-                onPressed: controller.payCartaoCredito,
-                // onPressed: controller.tipoTransacaoEntity
-                //         .where((element) =>
-                //             element.tipoTransacao == TipoTransacao.CREDITO)
-                //         .isNotEmpty
-                //     ? controller.payCartaoCredito
-                //     : null,
+                onPressed: controller.tipoTransacaoEntity
+                        .where((element) =>
+                            element.tipoTransacao == TipoTransacao.CREDITO)
+                        .isNotEmpty
+                    ? controller.payCartaoCredito
+                    : null,
               ),
               Spacer(flex: 8),
               BtnPrimary(
